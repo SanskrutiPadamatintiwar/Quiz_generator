7f201c5b91c094f3d40f0bed345c2b301ceecfc0
SanskrutiPadamatintiwar <147150465+SanskrutiPadamatintiwar@users.noreply.github.com>
2024-12-12 15:21:23 +0530

    added files

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import logo from "./logo.svg";
+import qimg from "./qImg.jpg";
+import navimg from "./navImg.jpg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import QuizPage from "./components/QuizPage";
+import AboutUs from "./components/AboutUs";
+import Questions from "./components/Questions";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+function App() {
+  
+  return (
+    <Router>
+    <div className="bg-no-repeat bg-cover bg-center min-h-screen relative flex flex-col " style={{ backgroundImage: `url(${qimg})` }}>
+     
+      <Navbar />
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/quiz" element={<QuizPage />}/>
+      <Route path="/about" element={<AboutUs />} />
+      <Route path="/questions" element={<Questions />} />
+      </Routes>
+      {/* <Home/> */}
+    </div>
+
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Questions.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Questions = () => {
+  return (
+    <div>
+        <div className="md mt-12 max-w-3xl flex px-10 py-10 items-center justify-center">
+
+            <h1>Questions will be displayed here</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Questions
